d80c92982ee6f93d7d28e205871ed647f1b0af91
Erase <73711284+Erase01@users.noreply.github.com>
2023-05-23 08:37:27 +0200

    Update SerialRead file

--- Raspi_Setup/SerialRead.py
+++ Raspi_Setup/SerialRead.py
@@ -1,4 +1,10 @@
 import serial
 
-if __name__ == "__main__":
-    
+def serRead(signal):
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer()
+
+    while True:
+        if ser.in_waiting < 0:
+            signal = ser.readlines().decode('utf-8')
+            return signal
